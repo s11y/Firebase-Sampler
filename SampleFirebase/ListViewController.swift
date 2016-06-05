fdab4f4d37f7d476f9e72164e89daa8c68d36c31
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-06 05:44:42 +0900

    [Add] Login comment

--- SampleFirebase/ListViewController.swift
+++ SampleFirebase/ListViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Firebase
+import Firebase //Firebaseをインポート
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
--- SampleFirebase/LoginViewController.swift
+++ SampleFirebase/LoginViewController.swift
@@ -7,39 +7,46 @@
 //
 
 import UIKit
-import Firebase
+import Firebase //Firebaseをインポート
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField! // Emailを打つためのTextField
     
-    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField! //Passwordを打つためのTextField
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        emailTextField.delegate = self
-        passwordTextField.delegate = self
+        emailTextField.delegate = self //デリゲートをセット
+        passwordTextField.delegate = self //デリゲートをセット
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    //ログインボタン
     @IBAction func didRegisterUser() {
+        //ログインのためのメソッド
         login()
     }
-    
+    //Returnキーを押すと、キーボードを隠す
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-    
+    //ログインのためのメソッド
     func login() {
+        //EmailとPasswordのTextFieldに文字がなければ、その後の処理をしない
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
+        
+        //signInWithEmailでログイン
+        //第一引数にEmail、第二引数にパスワードを取ります
         FIRAuth.auth()?.signInWithEmail(email, password: password, completion: { (user, error) in
+            //エラーなしなら、ログイン完了
             if error == nil{
                 print(FIRAuth.auth()?.currentUser)
                 self.transitionToView()
@@ -48,7 +55,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             }
         })
     }
-    
+    //ログイン完了後に、ListViewControllerへの遷移のためのメソッド
     func transitionToView()  {
         self.performSegueWithIdentifier("toVC", sender: self)
     }
