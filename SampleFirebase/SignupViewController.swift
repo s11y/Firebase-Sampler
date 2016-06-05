404d4621234fc0eb3609526a0e9e8280a3af6cae
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-06 05:41:53 +0900

    [Add] signup comment out

--- SampleFirebase/SignupViewController.swift
+++ SampleFirebase/SignupViewController.swift
@@ -7,25 +7,27 @@
 //
 
 import UIKit
-import Firebase
+import Firebase //Firebaseをインポート
 
 class SignupViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField! // Emailを打つためのTextField
     
-    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField! //Passwordを打つためのTextField
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        emailTextField.delegate = self
-        passwordTextField.delegate = self
+        emailTextField.delegate = self //デリゲートをセット
+        passwordTextField.delegate = self //デリゲートをセット
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
+        //ログインしていれば、遷移
+        //FIRAuthがユーザー認証のためのフレーム
+        //FIRAuth.auth()?.currentUserにログインしているユーザーの情報が入ってます。
         if FIRAuth.auth()?.currentUser != nil{
             self.transitionToView()
         }
@@ -36,19 +38,24 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    //サインアップボタン
     @IBAction func willSignup() {
+        //サインアップのための関数
         signup()
     }
-    
+    //ログイン画面への遷移ボタン
     @IBAction func willTransitionToLogin() {
         transitionToLogin()
     }
-    
+    //Signupのためのメソッド
     func signup() {
+        //emailTextFieldとpasswordTextFieldに文字がなければ、その後の処理をしない
         guard let email = emailTextField.text else  { return }
         guard let password = passwordTextField.text else { return }
+        //FIRAuth.auth()?.createUserWithEmailでサインアップ
+        //第一引数にEmail、第二引数にパスワード
         FIRAuth.auth()?.createUserWithEmail(email, password: password, completion: { (user, error) in
+            //エラーなしなら、認証完了
             if error == nil{
                 self.transitionToView()
             }else {
@@ -56,15 +63,15 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
             }
         })
     }
-    
+    //ログイン画面への遷移
     func transitionToLogin() {
         self.performSegueWithIdentifier("toLogin", sender: self)
     }
-    
+    //ListViewControllerへの遷移
     func transitionToView() {
         self.performSegueWithIdentifier("toView", sender: self)
     }
-    
+    //Returnキーを押すと、キーボードを隠す
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
