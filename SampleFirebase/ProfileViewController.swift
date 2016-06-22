39f5ac1266179962b29989d9cd840d6b6fddcf0b
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-22 18:00:33 +0900

    [Add] UIAlertController

--- SampleFirebase/ProfileViewController.swift
+++ SampleFirebase/ProfileViewController.swift
@@ -7,17 +7,54 @@
 //
 
 import UIKit
+import Firebase
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var emailLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.updateProfileImageView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didSelectPlus() {
+        self.presentActionSheet()
+    }
+    
+    func presentActionSheet() {
+        let alert = UIAlertController(title: "写真を選択", message: "どちらから選択しますか?", preferredStyle: .ActionSheet)
+        let library = UIAlertAction(title: "ライブラリ", style: .Default) { (action) in
+            self.presentPhotoLibrary()
+        }
+        
+        let camera = UIAlertAction(title: "カメラ", style: .Default) { (action) in
+            self.presentCamera()
+        }
+        
+        alert.addAction(library)
+        alert.addAction(camera)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func presentPhotoLibrary() {
+        
+    }
+    
+    func presentCamera() {
+        
+    }
+    
+    func updateProfileImageView() {
+        profileImageView.layer.cornerRadius = profileImageView.layer.bounds.width/2
+        profileImageView.layer.masksToBounds = true
     }
 }
