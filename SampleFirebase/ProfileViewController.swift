cac5113d7ae43bc9fd090336a2d70a5cf1a958a7
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-22 17:28:26 +0900

    [Create] Profile View Controller

---
+++ SampleFirebase/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  SampleFirebase
+//
+//  Created by ShinokiRyosei on 2016/06/22.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
